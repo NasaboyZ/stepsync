5df602324a5ea8a8b242018c6b7d08ee1f17e29e
NazaboyZ <josefleite.00@hotmail.com>
2025-01-25 10:19:59 +0100

    challenges Counter implemented

--- frontend/src/components/dashboardItems/dashboarditems.tsx
+++ frontend/src/components/dashboardItems/dashboarditems.tsx
@@ -1,4 +1,5 @@
 import DashboardBmi from "../bmiCard/dashboardBmi";
+import ChallengeCounter from "../challengCounter/challengeCounter";
 import DashboardCards from "../dashboardCards/dashboardCards";
 import ProfileBioCard from "../profileBioCard/profileBioCard";
 import WorkoutChart from "../workoutChart/workoutchart";
@@ -6,6 +7,7 @@ import WorkoutChart from "../workoutChart/workoutchart";
 export default function DashboardItems() {
   return (
     <>
+      <ChallengeCounter />
       <WorkoutChart />
       <DashboardBmi />
       <DashboardCards />
