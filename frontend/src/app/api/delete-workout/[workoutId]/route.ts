e45a6bfd73534cec41c2790e7028519ea9624003
NazaboyZ <josefleite.00@hotmail.com>
2025-02-12 18:05:28 +0100

    fixt delete bug from build avatar

--- frontend/src/app/api/delete-workout/[workoutId]/route.ts
+++ frontend/src/app/api/delete-workout/[workoutId]/route.ts
@@ -2,52 +2,44 @@ import { authConfig } from "@/auth";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { workoutId: number } }
-) {
-  if (request.method !== "DELETE") {
-    return NextResponse.json(
-      { message: "Method not allowed" },
-      { status: 405 }
-    );
-  }
+type Props = {
+  params: { workoutId: string };
+};
 
+export async function DELETE(_request: NextRequest, { params }: Props) {
   const session = await getServerSession(authConfig);
   if (!session || !session.accessToken) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
-  const workoutId = params.workoutId;
 
   try {
+    const workoutId = parseInt(params.workoutId);
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/workouts?id=${workoutId}`,
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/workouts/${workoutId}`,
       {
         method: "DELETE",
         headers: {
-          "Content-Type": "application/json",
           Authorization: `Bearer ${session.accessToken}`,
         },
-        body: JSON.stringify({ id: workoutId }),
       }
     );
 
     if (!response.ok) {
       const error = await response.json();
       return NextResponse.json(
-        { message: "workout deletion failed", error },
+        { message: "Workout deletion failed", error },
         { status: response.status }
       );
     }
 
     return NextResponse.json(
-      { message: "workout deletion successful" },
+      { message: "Workout deletion successful" },
       { status: 200 }
     );
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { message: "workout deletion failed" },
+      { message: "Workout deletion failed" },
       { status: 500 }
     );
   }
