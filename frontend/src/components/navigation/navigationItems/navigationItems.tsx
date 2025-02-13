c62de8ed57cabc97f196477359839e1906cd4a95
NazaboyZ <josefleite.00@hotmail.com>
2025-02-13 21:15:03 +0100

    underline stylish aktiv state

--- frontend/src/components/navigation/navigationItems/navigationItems.tsx
+++ frontend/src/components/navigation/navigationItems/navigationItems.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
 import Style from "./navigationItems.module.css";
+import { usePathname } from "next/navigation";
 
 interface NavigationItemProps {
   items: { href: string; label: string }[];
 }
 
 export default function NavigationItem({ items }: NavigationItemProps) {
+  const pathname = usePathname();
+
   return (
     <ul className={Style["navItem_ul"]}>
       {items.map((item, index) => (
         <li className={Style["navItem_li"]} key={index}>
-          <Link href={item.href}>{item.label}</Link>
+          <Link
+            href={item.href}
+            className={pathname === item.href ? Style["active"] : ""}
+          >
+            {item.label}
+          </Link>
         </li>
       ))}
     </ul>
