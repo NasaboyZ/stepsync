1d5251562730c3f357e024a10cf9668452582e86
NazaboyZ <josefleite.00@hotmail.com>
2025-02-14 14:44:28 +0100

    seeder für challenges entfernt

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -39,50 +39,5 @@ class DatabaseSeeder extends Seeder
             'gender'        => 'female',
             'date_of_birth' => '1992-05-15',
         ]);
-
-
-
-
-
-
-
-        // Create challenges
-        $challenge1 = Challenges::create([
-            'title'       => '10k Steps Daily',
-            'description' => 'Walk at least 10,000 steps daily for a week.',
-            'status'      => 'pending',
-            'start_date'  => '2024-12-29 13:21:00',
-            'end_date'    => '2025-01-05 13:21:00'
-        ]);
-
-        $challenge2 = Challenges::create([
-            'title'       => 'Drink 2L Water',
-            'description' => 'Drink at least 2 liters of water each day for a month.',
-            'status'      => 'pending',
-            'start_date'  => '2024-12-29 13:21:00',
-            'end_date'    => '2025-01-29 13:21:00'
-        ]);
-
-        $challenge3 = Challenges::create([
-            'title'       => '30-Day Yoga Challenge',
-            'description' => 'Practice yoga every day for 30 days.',
-            'status'      => 'pending',
-            'start_date'  => '2024-12-29 13:21:00',
-            'end_date'    => '2025-01-29 13:21:00'
-        ]);
-
-        // Alle Challenges allen Nutzern zuweisen mit individuellem Status
-        $users = [$alpha, $bravo]; // Array mit allen Nutzern
-        $challenges = [$challenge1, $challenge2, $challenge3]; // Array mit allen Challenges
-
-        foreach ($users as $user) {
-            foreach ($challenges as $challenge) {
-                $user->challenges()->attach($challenge->id, ['status' => 'pending']);
-            }
-        }
-
-        // Die alten Challenge-Zuweisungen entfernen
-        // $alpha->challenges()->sync([$challenge1->id]);
-        // $bravo->challenges()->sync([$challenge2->id, $challenge3->id]);
     }
 }
